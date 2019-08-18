cbebaa72cd51df903fb5a596e5460c7086ff65c4
Paul Florence <perso@florencepaul.com>
2019-08-18 10:28:30 +0100

    Describe Peertube API through a struct

--- src/peertube_api.rs
+++ src/peertube_api.rs
@@ -39,13 +39,14 @@ pub struct Video {
         serialize = "originallyPublishedAt",
         deserialize = "originallyPublishedAt"
     ))]
-    pub originally_published_at: String,
-    pub category: String,
-    pub licence: String,
-    pub language: String,
-    pub privacy: String,
+    pub originally_published_at: Option<String>,
+    pub category: Category,
+    pub licence: Licence,
+    pub language: Language,
+    pub privacy: Privacy,
     pub description: String,
     pub duration: u64,
+    #[serde(rename(serialize = "isLocal", deserialize = "isLocal"))]
     pub is_local: bool,
     #[serde(rename(serialize = "thumbnailPath", deserialize = "thumbnailPath"))]
     pub thumbnail_path: String,
@@ -57,11 +58,12 @@ pub struct Video {
     pub likes: u64,
     pub dislikes: u64,
     pub nsfw: bool,
-    pub wait_transcoding: bool,
-    pub state: String,
-    pub blacklisted: bool,
+    #[serde(rename(serialize = "waitTranscoding", deserialize = "waitTranscoding"))]
+    pub wait_transcoding: Option<bool>,
+    pub state: Option<State>,
+    pub blacklisted: Option<bool>,
     #[serde(rename(serialize = "blacklistedReason", deserialize = "blacklistedReason"))]
-    pub blacklisted_reason: String,
+    pub blacklisted_reason: Option<String>,
     pub account: Account,
     pub channel: Channel,
 }
@@ -86,4 +88,32 @@ pub struct Channel {
     pub url: String,
     pub host: String,
     pub avatar: Avatar,
+}
+
+macro_rules! peertube_field {
+    ($name:ident, $id_type:ident) => {
+        #[derive(Serialize, Deserialize, Debug, Hash)]
+        pub struct $name {
+            pub id: Option<$id_type>,
+            pub label: String,
+        }
+    };
+}
+
+peertube_field!(Category, u64);
+peertube_field!(Language, String);
+peertube_field!(Privacy, u64);
+peertube_field!(Licence, u64);
+peertube_field!(State, u64);
+
+#[cfg(test)]
+mod test {
+    use crate::peertube_api::Video;
+
+    #[test]
+    fn peertube_api() {
+        let json = include_str!("../tests/video1.json");
+        let video: Video = serde_json::from_str(json).unwrap();
+        println!("{:?}", video);
+    }
 }
